dbac0251b3c172266012f1557ef99def9f00d14f
Maxim <endurancemayer@yandex.ru>
2017-03-31 22:03:48 +0500

    First draft of database

--- support_handlers.py
+++ support_handlers.py
@@ -1,4 +1,5 @@
 import sys
+import dbm
 
 
 def print_auto_complete_variants(possible_values):
@@ -17,3 +18,41 @@ def assert_ascii(given_name):
     except UnicodeEncodeError as unicode_error:
         print("Sorry, ascii symbols supported only")
         sys.exit()
+
+
+class DataBase:
+    def __init__(self):
+        self.db = "records"
+
+    def add_record(self, string_name, string_description):
+        with dbm.open(self.db, "c") as db:
+            if string_name.encode() in db.keys():
+                return False
+            db[string_name] = string_description
+            return True
+
+    def get_record(self, string_name):
+        with dbm.open(self.db, "c") as db:
+            if string_name.encode() in db.keys():
+                return db[string_name].decode()
+
+    def get_records_count(self):
+        with dbm.open(self.db, "c") as db:
+            return len(db)
+
+    def delete_record(self, string_name):
+        with dbm.open(self.db, "c") as db:
+            if string_name.encode() in db.keys():
+                del db[string_name.encode()]
+                return True
+        return False
+
+    def retrieve_past_queries(self):
+        with dbm.open(self.db, "c") as db:
+            return "".join(db[x].decode() for x in db.keys())
+
+
+
+
+
+
